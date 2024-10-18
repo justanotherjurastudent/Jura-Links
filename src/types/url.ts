7ee985c1dee9a22eac39354947f53a56981fb844
Lukas Collier <129078768+justanotherjurastudent@users.noreply.github.com>
2024-10-18 19:43:46 +0200

    Add files via upload

--- src/types/url.ts
+++ src/types/url.ts
@@ -10,6 +10,8 @@ enum DejureUrl {
 	LAW = "https://www.dejure.org/gesetze/",
 	CASE = "https://www.dejure.org/dienste/vernetzung/rechtsprechung?Text=",
 	JOURNAL = "https://www.dejure.org/dienste/vernetzung/rechtsprechung?Text=",
+	BTDRUCKSACHE = "https://dejure.org/Drucksachen/Bundestag/",
+	BRDRUCKSACHE = "https://dejure.org/Drucksachen/Bundesrat/",
 }
 
 export { LawProviderUrl, DejureUrl };
